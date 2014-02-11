a132fc1f11b6ef453827cc727daa4dd62604a957
Ryan <yuwei.ryan@snda.com>
2014-02-11 16:43:56 +0800

    完成单元测试用例
    补充所有必要的单元测试用例

--- Hold'em/test/test_pokercard.rb
+++ Hold'em/test/test_pokercard.rb
@@ -7,40 +7,75 @@ require '/Workshop/workspace/GITRep/github/rep1/QA-Test-Repository/Hold\'em/card
 
 class TestPokercard < Test::Unit::TestCase
   
-    type = 'space'
-    Cardary_s = ["spade cards", Card.new(['ace', type]), Card.new(['two', type]), Card.new(['three', type]), Card.new(['four', type]), Card.new(['five', type]), Card.new(['six',type]), Card.new(['seven',type]), Card.new(['eight',type]), Card.new(['nine',type]), Card.new(['ten',type]), Card.new(['jack',type]), Card.new(['queen',type]), Card.new(['king',type])]
+    Cardary_s = []; type = 'spade'
+    Card::POINTS.to_a.reverse.each do |point, value|
+    	#puts point
+    	Cardary_s << Card.new([point, type])
+    end
+    	
+    Cardary_h = []; type = 'heart'
+    Card::POINTS.to_a.reverse.each do |point, value|
+    	Cardary_h << Card.new([point, type])
+    end
+    	
+    Cardary_d = []; type = 'diamond'
+    Card::POINTS.to_a.reverse.each do |point, value|
+    	Cardary_d << Card.new([point, type])
+    end
+    	
+    Cardary_c = []; type = 'club'
+    Card::POINTS.to_a.reverse.each do |point, value|
+    	Cardary_c << Card.new([point, type])
+    end
+    	
+  def test_is_flush_staight
         
-    type = 'heart'
-    Cardary_h = ["heart cards", Card.new(['ace', type]), Card.new(['two', type]), Card.new(['three', type]), Card.new(['four', type]), Card.new(['five', type]), Card.new(['six',type]), Card.new(['seven',type]), Card.new(['eight',type]), Card.new(['nine',type]), Card.new(['ten',type]), Card.new(['jack',type]), Card.new(['queen',type]), Card.new(['king',type])]
+    cardcomp1 = CardComposition.new([Cardary_s[6], Cardary_s[5], Cardary_s[4], Cardary_s[3], Cardary_s[7]])
+    cardcomp2 = CardComposition.new([Cardary_d[11], Cardary_d[13], Cardary_d[14], Cardary_d[12], Cardary_d[10]])
     
-    type = 'diamond'
-    Cardary_d = ["diamond cards", Card.new(['ace', type]), Card.new(['two', type]), Card.new(['three', type]), Card.new(['four', type]), Card.new(['five', type]), Card.new(['six',type]), Card.new(['seven',type]), Card.new(['eight',type]), Card.new(['nine',type]), Card.new(['ten',type]), Card.new(['jack',type]), Card.new(['queen',type]), Card.new(['king',type])]
-    
-    type = 'club'
-    Cardary_c = ["club cards", Card.new(['ace', type]), Card.new(['two', type]), Card.new(['three', type]), Card.new(['four', type]), Card.new(['five', type]), Card.new(['six',type]), Card.new(['seven',type]), Card.new(['eight',type]), Card.new(['nine',type]), Card.new(['ten',type]), Card.new(['jack',type]), Card.new(['queen',type]), Card.new(['king',type])]
-
-  def test_is_twopair
-    
-    
-    cardcomp1 = CardComposition.new([TestPokercard::Cardary_s[2], TestPokercard::Cardary_s[7], TestPokercard::Cardary_d[8], TestPokercard::Cardary_c[2], TestPokercard::Cardary_h[7]])
-    cardcomp2 = CardComposition.new([TestPokercard::Cardary_s[1], TestPokercard::Cardary_s[3], TestPokercard::Cardary_s[4], TestPokercard::Cardary_h[7], TestPokercard::Cardary_s[7]])
-    
-    assert_equal("Two Pair", cardcomp1.set_value_type)
-    assert_not_equal("Two Pair", cardcomp2.set_value_type)
+    assert_equal("Flush Straight", cardcomp1.set_value_type, "It should be Flush Straight")
+    assert_equal("Flush Straight", cardcomp2.set_value_type, "It should be Loyal Flush Straight")
     
   end
-  
+
+  def test_is_four_kind
+        
+    cardcomp1 = CardComposition.new([Cardary_s[9], Cardary_h[9], Cardary_d[9], Cardary_c[9], Cardary_c[7]])
+    
+    assert_equal("Four Kinds", cardcomp1.set_value_type, "It should be Four of a kind 9")
+    
+  end
+
+  def test_is_full_house
+    cardcomp1 = CardComposition.new([Cardary_s[2], Cardary_h[2], Cardary_d[8], Cardary_c[2], Cardary_h[8]])
+    cardcomp2 = CardComposition.new([Cardary_s[2], Cardary_h[2], Cardary_d[8], Cardary_c[2], Cardary_h[7]])
+    
+    assert_equal("Full House", cardcomp1.set_value_type, "Should be full of house of 2 with 8")
+    assert_equal("Three Kinds", cardcomp2.set_value_type, "Should be three of a kind of 2")
+  	
+  end
+
   def test_is_straight
         
-    cardcomp1 = CardComposition.new([TestPokercard::Cardary_s[2], TestPokercard::Cardary_s[6], TestPokercard::Cardary_d[5], TestPokercard::Cardary_c[4], TestPokercard::Cardary_h[3]])
-    cardcomp2 = CardComposition.new([TestPokercard::Cardary_s[1], TestPokercard::Cardary_s[3], TestPokercard::Cardary_s[4], TestPokercard::Cardary_h[7], TestPokercard::Cardary_s[7]])
-    cardcomp3 = CardComposition.new([TestPokercard::Cardary_s[1], TestPokercard::Cardary_s[2], TestPokercard::Cardary_d[5], TestPokercard::Cardary_c[4], TestPokercard::Cardary_h[3]])
+    cardcomp1 = CardComposition.new([Cardary_s[2], Cardary_s[6], Cardary_d[5], Cardary_c[4], Cardary_h[3]])
+    cardcomp2 = CardComposition.new([Cardary_s[1], Cardary_s[3], Cardary_s[4], Cardary_h[7], Cardary_s[7]])
+    cardcomp3 = CardComposition.new([Cardary_s[1], Cardary_s[2], Cardary_d[5], Cardary_c[4], Cardary_h[3]])
     
-    assert_equal("Straight", cardcomp1.set_value_type)
-    assert_not_equal("Straight", cardcomp2.set_value_type)
-    assert_equal("Straight", cardcomp3.set_value_type)    
+    assert_equal("Straight", cardcomp1.set_value_type, "normal straight: 2-3-4-5-6")
+    assert_not_equal("Straight", cardcomp2.set_value_type, "This card composition should not be straight")
+    assert_equal("Straight", cardcomp3.set_value_type, "Straight with A: A-2-3-4-5")    
     
   end
   
+  def test_is_pair
+    
+    
+    cardcomp1 = CardComposition.new([Cardary_s[2], Cardary_s[7], Cardary_d[8], Cardary_c[2], Cardary_h[7]])
+    cardcomp2 = CardComposition.new([Cardary_s[1], Cardary_s[3], Cardary_s[4], Cardary_h[7], Cardary_s[7]])
+    
+    assert_equal("Two Pair", cardcomp1.set_value_type, "It should be two pairs")
+    assert_equal("Pair", cardcomp2.set_value_type, "It should be pair")
+    
+  end
   
 end
